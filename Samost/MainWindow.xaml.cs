ec10f4606ea157027dc022ceb10e216b98ea2d95
PoznyakM <mikklepoz2002@gmail.com>
2021-09-25 15:31:56 +0300

    Основы WPF. Работа с элементами управления
    25 сентября, все работы

---
+++ Samost/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Samost
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool Sum = true;
+        public int? FirstInt;
+        public int? SecondInt;
+        public int? ThirdInt;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (FirstInt != null && SecondInt != null && ThirdInt != null)
+            {
+                if (Sum == true)
+                {
+                    Answer.Text = (FirstInt + SecondInt + ThirdInt).ToString();
+                    Sum = false;
+                    but1.Content = "Вывести произведение";
+                }
+                else
+                {
+                    Answer.Text = (FirstInt * SecondInt * ThirdInt).ToString();
+                    Sum = true;
+                    but1.Content = "Вывести сумму";
+                }
+            }
+        }
+
+        private void First_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FirstInt = Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void Second_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            SecondInt = Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void Third_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ThirdInt = Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void First_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Char.IsDigit(e.Text, 0);
+        }
+
+        private void Second_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Char.IsDigit(e.Text, 0);
+        }
+
+        private void Third_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Char.IsDigit(e.Text, 0);
+        }
+    }
+}
---
+++ Samost2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Samost2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double Bytes;
+        public double KBytes;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InBytes_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !((Char.IsDigit(e.Text, 0) || ((e.Text == System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString()))));
+        }
+
+        private void InKBytes_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !((Char.IsDigit(e.Text, 0) || ((e.Text == System.Globalization.CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator[0].ToString()))));
+        }
+
+        private void InBytes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Bytes = Convert.ToDouble((sender as TextBox).Text);
+            
+        }
+
+        private void but1_Click(object sender, RoutedEventArgs e)
+        {
+            InKBytes.Text = (Bytes / 1024).ToString();
+        }
+    }
+}
