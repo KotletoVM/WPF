940d758eb7d8552e32b85be7bab095514ff41e3b
PoznyakM <vash.lavashik@gmail.com>
2021-10-26 19:07:37 +0300

    API1 + ToursWebAPI

---
+++ API/ToursWebApi/Models/ResponceHotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToursWebApi.Entities;
+
+namespace ToursWebApi.Models
+{
+    public class ResponceHotel
+    {
+        public ResponceHotel(Hotel hotel)
+        {
+            Id = hotel.Id;
+            Name = hotel.Name;
+            CountOfStars = hotel.CountOfStars;
+            CountryName = hotel.CountryCode;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountOfStars { get; set; }
+        public string CountryName { get; set; }
+
+    }
+}
